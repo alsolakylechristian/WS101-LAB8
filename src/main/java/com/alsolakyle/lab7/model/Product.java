f37a71371d16db9099b1a3f96a567379e5eb6897
alsolakylechristian <alsolakylechristian@gmail.com>
2025-12-05 03:03:12 +0800

    Update Product.java

--- src/main/java/com/alsolakyle/lab7/model/Product.java
+++ src/main/java/com/alsolakyle/lab7/model/Product.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data // Generates getters, setters, toString, etc. [cite: 238]
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-    private Long id;     // Unique identifier [cite: 223]
-    private String name; // Name of the product [cite: 223]
-    private Double price; // Price of the product [cite: 223]
+    private Long id;     // Unique identifier
+    private String name; // Name of the product
+    private Double price; // Price of the product
 }
